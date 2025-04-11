1dd2f26043c766161a32f150d3a49b1ba6a53597
MeetJain0170 <meetjain1333@gmail.com>
2025-04-11 05:40:32 +0530

    Jump Game

---
+++ GreedyAlgorithm/JumpGame.java
@@ -0,0 +1,15 @@
+public class JumpGame {
+    public static boolean canJump(int []nums){
+        int n=nums.length;
+        int far=0;
+        for(int i=0; i<n; i++){
+            if(i>far){
+                return false;
+            }
+            far = Math.max(far, i+nums[i]);
+            if(far>=n-1){
+                return true;
+            }
+        }return false;
+    }
+}
