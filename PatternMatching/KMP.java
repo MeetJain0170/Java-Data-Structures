5a8c8b546166a2fc3ba873ea979ec590f107ee59
MeetJain0170 <meetjain1333@gmail.com>
2025-04-20 22:21:52 +0530

    Knuth-Morris-Pratt string matching implementation

---
+++ PatternMatching/KMP.java
@@ -0,0 +1,50 @@
+public class KMP {
+    public static void KMPSearch(String pattern, String text) {
+        int m = pattern.length();
+        int n = text.length();
+        int[] lps = computeLPSArray(pattern);
+
+        int i = 0, j = 0;
+        while (i < n) {
+            if (pattern.charAt(j) == text.charAt(i)) {
+                i++;
+                j++;
+            }
+            if (j == m) {
+                System.out.println("Pattern found at index: " + (i - j));
+                j = lps[j - 1];
+            } else if (i < n && pattern.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+    }
+
+    private static int[] computeLPSArray(String pattern) {
+        int m = pattern.length();
+        int[] lps = new int[m];
+        int len = 0;
+        lps[0] = 0;
+        int i = 1;
+        while (i < m) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i++] = len;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i++] = 0;
+                }
+            }
+        }
+        return lps;
+    }
+
+    public static void main(String[] args) {
+        KMPSearch("ABCDX", "ABCDABCDX");
+    }
+}
